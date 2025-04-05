f14884ca68efa01ed7fc8baa999f4ba8710da041
Justin <zooecostudy@yahoo.com>
2025-04-04 20:32:40 -0600

    room.cpp fix

--- room.cpp
+++ room.cpp
@@ -1,7 +1,7 @@
 #include "room.h"
 #include <iostream>
 
-Room::Room(std::string roomDescription) : description(roomDescription), enemy(nullptr) {}
+Room::Room(std::string roomDescription) : description(roomDescription), enemy(nullptr), hasArtifact(false), north(nullptr), south(nullptr), east(nullptr), west(nullptr) {}
 
 void Room::addOption(std::string option) {
     options.push_back(option);
@@ -13,8 +13,13 @@ void Room::showRoom() {
     for (int i = 0; i < options.size(); i++) {
         std::cout << i + 1 << ". " << options[i] << "\n";
     }
+    std::cout << "You can also move north, south, east, or west.\n";
 }
 
 void Room::setEnemy(Enemy* newEnemy) {
     enemy = newEnemy;
+}
+
+void Room::setArtifact(bool artifact) {
+    hasArtifact = artifact;
 }
